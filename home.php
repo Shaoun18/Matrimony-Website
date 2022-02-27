1a4e079f75659f57dfaaa368b794f9e804a3054e
Shaoun18 <shaounchandrashill75@gmail.com>
2022-02-27 20:31:28 +0600

    Matrimony Website Project

---
+++ home.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+   header('location:login1.html');
+
+}
+?>
---
+++ profile.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <tittle><h3></h3></tittle>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/all.min.css" />
+    <link rel="stylesheet" href="css/pro-bars.min.css" />
+    <link rel="stylesheet" href="css/slick.css" />
+    <!-- <link rel="stylesheet" href="css/lightbox.min.css" /> -->
+    <link rel="stylesheet" href="css/venobox.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+  </head>
+
+  <body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="images/zibonshathi.png" alt="logo" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i class="fas fa-bars"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#"
+                >Home <span class="sr-only">(current)</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#maleFeatured">Members</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Profile.php">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href = "logout.php">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!--Navbar part end -->
+   
+    </html>
